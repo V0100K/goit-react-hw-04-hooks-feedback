54f42300b1d769f5ec5b994aefdf1a770b3124ee
Vitaliy  Malyshko <malyshko2001@gmail.com>
2021-12-17 00:11:00 +0200

    rename

--- src/components/feedback/Feedback.jsx
+++ src/components/feedback/Feedback.jsx
@@ -1,26 +1,25 @@
-import React, {useState} from "react";
+import React, { useState } from "react";
 import FeedbackOptions from "../feedbackOptions/";
 import Section from "../section/";
 import Statistics from "../statistics/";
 
 const Feedback = () => {
-  const [good, setGood]=useState(0);
-  const [neutral, setNeutral]=useState(0);
-  const [bad, setBad]=useState(0);
-
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
 
   const handleInc = (event) => {
     const name = event.target.name;
 
     switch (name) {
-      case 'good':
-        setGood(prevState => prevState + 1);
+      case "good":
+        setGood((prevState) => prevState + 1);
         return;
-      case 'neutral':
-        setNeutral(prevState => prevState + 1);
+      case "neutral":
+        setNeutral((prevState) => prevState + 1);
         return;
-      case 'bad':
-        setBad(prevState => prevState + 1);
+      case "bad":
+        setBad((prevState) => prevState + 1);
         return;
 
       default:
@@ -28,48 +27,40 @@ const Feedback = () => {
     }
   };
 
-
-
-
   const countTotalFeedback = () => {
     return good + neutral + bad;
   };
-
-// WArning!!!!
-//   const result = Math.round(
-//       (good / countTotalFeedback()) * 100
-//     );
-//     return ;
-//   };
-
+  // WArning!!!!
+  //   const result = Math.round(
+  //       (good / countTotalFeedback()) * 100
+  //     );
+  //     return ;
+  //   };
   const countPositiveFeedbackPercentage = () => {
-    return Math.round(
-        (good / countTotalFeedback()) * 100
-    );
+    return Math.round((good / countTotalFeedback()) * 100);
   };
 
-    return (
-      <>
-        <Section title="Please leave feedback">
-          <FeedbackOptions handleInc={handleInc} />
-        </Section>
+  return (
+    <>
+      <Section title="Please leave feedback">
+        <FeedbackOptions handleInc={handleInc} />
+      </Section>
 
-        <Section title="Statistics">
-          {countTotalFeedback() ? (
-            <Statistics
-              good={good}
-              neutral={neutral}
-              bad={bad}
-              countTotalFeedback={countTotalFeedback}
-              countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
-            />
-          ) : (
-            <p>No feedback given</p>
-          )}
-        </Section>
-      </>
-    );
-
-}
+      <Section title="Statistics">
+        {countTotalFeedback() ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            countTotalFeedback={countTotalFeedback}
+            countPositiveFeedbackPercentage={countPositiveFeedbackPercentage}
+          />
+        ) : (
+          <p>No feedback given</p>
+        )}
+      </Section>
+    </>
+  );
+};
 
 export default Feedback;
